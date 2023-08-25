09c3e8fc07d3fcadfe59792d4730ee817cf7bd80
Jiali <JIALI.JIN@weboniselab.com>
2023-08-25 16:09:23 -0300

    configure avalara axios instance

---
+++ app/services/avalara-service.js
@@ -0,0 +1,37 @@
+const axios = require('axios');
+
+const { AVALARA_AUTH } = process.env;
+/**
+ * URL is https://api.bigcommerce.com/stores/${store_hash}/v3/tax/providers/${provider_id}/connection
+ * bcConnectionClient instance is used for Tax Provider Connection
+ */
+const avalaraInstance = axios.create({
+  baseURL: 'https://rest.avatax.com/api/v2/',
+  timeout: 1000,
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+    Authorization: `Basic ${AVALARA_AUTH}`,
+    'X-Avalara-Client': 'taxWrapper;2023.0;test integration;WRL370PrajaktaT',
+  },
+});
+
+const getAvalaraService = async ({ url }) => {
+  try {
+    const res = await avalaraInstance.get(url);
+    return res;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const postAvalaraService = async ({ url, body }) => {
+  try {
+    const res = await avalaraInstance.post(url, body);
+    return res;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+module.exports = { avalaraInstance, getAvalaraService, postAvalaraService };
